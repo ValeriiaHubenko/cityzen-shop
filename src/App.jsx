bdc8ca34635064c4be676032fb0534ae2e46d2fd
Valeriia <valeria.hubenko@students.beaminstitute.org>
2024-07-18 20:10:13 +0200

    2 name updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Route, Switch } from "wouter";
 import { CartProvider } from "./components/CartContext";
+// import Header from './components/Header'
+// import Footer from './components/Footer';
+// import Subscribe from './components/Subscribe';
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import ProductInfo from "./pages/ProductInfo";
@@ -8,40 +11,53 @@ import Checkout from "./pages/Checkout";
 import SuccessfulPayment from "./pages/SuccessfulPayment";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+// import MenuProducts from './components/MenuProducts'
 import "./App.css";
 
-const base = "/cityzen";
+const base = "/cityzen-shop";
 
 function App() {
   return (
     <>
-      <CartProvider>
-        <Switch>
-          <Route path={`${base}/`}>
-            <Home />
-          </Route>
-          <Route path={`${base}/products`}>
-            <Products />
-          </Route>
-          <Route path={`${base}/productinfo/:id`}>
-            <ProductInfo />
-          </Route>
-          <Route path={`${base}/cart`}>
-            <Cart />
-          </Route>
-          <Route path={`${base}/checkout`}>
-            <Checkout />
-          </Route>
-          <Route path={`${base}/successfulpayment`}>
-            <SuccessfulPayment />
-          </Route>
-          <Route path={`${base}/about`}>
-            <About />
-          </Route>
-          <Route path={`${base}/contact`}>
-            <Contact />
-          </Route>
-        </Switch>
+    <CartProvider>
+      <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+
+        <Route path="/cityzen">
+          <Home />
+        </Route>
+
+        <Route path="/products">
+          <Products />
+        </Route>
+
+        <Route path="/productinfo/:id">
+          <ProductInfo />
+        </Route>
+
+        <Route path="/cart">
+          <Cart />
+        </Route>
+
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+
+        <Route path="/successfulpayment">
+          <SuccessfulPayment />
+        </Route>
+
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/contact">
+          <Contact />
+        </Route>
+         
+      </Switch>
       </CartProvider>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { Router } from "wouter";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const base = "/cityzen";
+const base = "/cityzen-shop";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
